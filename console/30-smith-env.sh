fee93d4ccb61ef1d386f873c7d01db646b357e4b
tester <tester@example.com>
2026-03-07 21:33:36 -0500

    scenario: single loop success

--- console/30-smith-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-
-: "${SMITH_API_BASE_URL:=http://localhost:8080}"
-export SMITH_API_BASE_URL
-
-envsubst '${SMITH_API_BASE_URL}' < /opt/smith/runtime-config.template.js > /tmp/runtime-config.js
--- console/runtime-config.template.js
+++
@@ -1,3 +0,0 @@
-window.__SMITH_CONFIG__ = {
-  apiBaseUrl: "${SMITH_API_BASE_URL}"
-};
